b97da9382aea3ffc17fa07d6ed47aaf442fae05f
Saad Khurshid Qurashi <saadkhurshidqureshi@gmail.com>
2024-12-17 00:25:45 +0500

    Adding Our Project

---
+++ Pizza.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    cout <<"Shumaz Saeed and Saad Khurshid Qurashi"<< endl;
+    cout << " Third Semester" << endl;
+    cout << "Pizza Shop Ordering System ---> ' DSA' Semester Project" << endl;
+    return 0;
+}
