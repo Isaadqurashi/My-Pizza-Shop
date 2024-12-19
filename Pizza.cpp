b907645f5bc9b65028883d2ce8012b1724d91000
Saad Khurshid Qurashi <saadkhurshidqureshi@gmail.com>
2024-12-19 15:09:29 +0500

    i guess... done everything

--- Pizza.cpp
+++
@@ -1,9 +0,0 @@
-#include <bits/stdc++.h>
-using namespace std;
-
-int main(){
-    cout <<"Shumaz Saeed and Saad Khurshid Qurashi"<< endl;
-    cout << " Third Semester" << endl;
-    cout << "Pizza Shop Ordering System ---> ' DSA' Semester Project" << endl;
-    return 0;
-}
